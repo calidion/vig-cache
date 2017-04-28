b8df9438b8060e2431fbccbd8a935ddfe3085488
calidion <calidion@gmail.com>
2017-04-29 02:44:45 +0800

    enabled kvcache

---
+++ src/cache.ts
@@ -0,0 +1,119 @@
+
+import * as crypto from "crypto";
+import * as redis from "redis";
+
+export class KVCache {
+  protected store;
+  protected timeout;
+  constructor(redis, timeout = 5 * 60) {
+    this.store = redis;
+    this.timeout = timeout;
+  }
+  public attach(app) {
+    app.use(async (req, res, next) => {
+      let expireTime = await this._get(req.originalUrl);
+      if (!expireTime) {
+        expireTime = new Date().getTime();
+        await this._set(req.originalUrl, expireTime);
+      }
+      const user = req.session ? req.session.user : undefined;
+      const data: any = await this.check(req, user);
+      req.cache = {
+        cache: this
+      };
+      if (data && data.data && (data.time > expireTime)) {
+        req.cache.data = data.data;
+      } else {
+        this.clear(req, user);
+      }
+      next();
+    });
+  }
+
+  public sign(params) {
+    const temp = this.marshall(params || {});
+    const tempBuff = new Buffer(temp);
+    const crypt = crypto.createHash("MD5");
+    crypt.update(tempBuff);
+    return crypt.digest("hex").toUpperCase();
+  }
+  public generate(req, user = null) {
+    const url = req.originalUrl;
+    const body = req.body;
+    const params = req.params;
+    const query = req.query;
+    const hashes: any = {
+      url: this.marshall(url),
+      body: this.marshall(body),
+      params: this.marshall(params),
+      query: this.marshall(query)
+    };
+    if (user) {
+      hashes.user = this.marshall({ id: user.id });
+    }
+    return this.sign(hashes);
+  }
+  public async check(req, user = null) {
+    const key = this.generate(req, user);
+    return await this._get(key);
+  }
+  public async get(req, user = null) {
+    const k = this.generate(req, user);
+    return await this._get(k);
+  }
+  public async set(req, user = null, data) {
+    const k = this.generate(req, user);
+    await this._set(k, {
+      data,
+      time: new Date().getTime()
+    });
+  }
+  public clear(req, user = null) {
+    const k = this.generate(req, user);
+    this._clear(k);
+  }
+
+  protected marshall(params) {
+    params = params || {};
+    const keys = Object.keys(params).sort();
+    const obj = {};
+    const kvs = [];
+    for (let i = 0; i < keys.length; i++) {
+      const k = keys[i];
+      if (params[k]) {
+        obj[k] = params[k];
+        kvs.push(keys[i] + "=" + params[k]);
+      }
+    }
+    return kvs.join("&");
+  }
+
+  protected _promise(resolve, reject, cb = null) {
+    return (error, data) => {
+      if (error) {
+        return reject(error);
+      }
+      if (cb instanceof Function && data) {
+        cb(data);
+      } else {
+        resolve();
+      }
+    };
+  }
+  protected _get(k) {
+    return new Promise((resolve, reject) => {
+      this.store.get(k, this._promise(resolve, reject, (data) => {
+        resolve(JSON.parse(data));
+      }));
+    });
+  }
+  protected _set(k, v) {
+    v = JSON.stringify(v);
+    return new Promise((resolve, reject) => {
+      this.store.set(k, v, "EX", this.timeout, this._promise(resolve, reject));
+    });
+  }
+  protected _clear(k) {
+    this.store.del(k);
+  }
+}
---
+++ test/cache-spec.ts
@@ -0,0 +1,125 @@
+
+import { KVCache } from "../src/cache";
+import * as config from "./config/redis";
+import * as Redis from "redis";
+import * as assert from "assert";
+import * as express from "express";
+import * as request from "supertest";
+
+let redis = Redis.createClient(config);
+let cache;
+let app = express();
+
+describe("Cache", () => {
+  it("should create kvCache", () => {
+    cache = new KVCache(redis);
+    cache.attach(app);
+    cache.clear('/', null);
+    cache.clear({
+      originalUrl: '/cache'
+    }, null);
+  });
+
+  it("should marshall an object", () => {
+    let a = cache.marshall({});
+    assert(a === '');
+    let b = cache.marshall({ a: 100 });
+    assert(b === 'a=100');
+  });
+
+  it("should marshall an object", () => {
+    let a = cache.marshall({});
+    assert(a === '');
+    let b = cache.marshall({ a: 100 });
+    assert(b === 'a=100');
+  });
+
+  it("should set a k", async () => {
+    await cache._set('a', 100);
+    let data = await cache._get('a');
+    assert(data === 100);
+    await cache._clear('a');
+    data = await cache._get('a');
+    console.log(data);
+    assert(typeof data === 'undefined');
+  });
+
+  it("should _promise", (done) => {
+    let func = cache._promise(null, function (error) {
+      assert(error);
+      done();
+    });
+    func(true);
+  });
+
+
+  it("should set req", async () => {
+    await cache.set({}, null, 100);
+    let data = await cache.get({});
+    console.log(data);
+    assert(data.data === 100);
+    assert(data.time);
+    await cache.clear({});
+    data = await cache.get({});
+    console.log(data);
+    assert(typeof data === 'undefined');
+  });
+
+  it("should set req with user", async () => {
+    await cache.set({}, { id: 1 }, 100);
+    let data = await cache.get({}, { id: 1 });
+    console.log(data);
+    assert(data.data === 100);
+    assert(data.time);
+    await cache.clear({}, { id: 1 });
+    data = await cache.get({}, { id: 1 });
+    console.log(data);
+    assert(typeof data === 'undefined');
+  });
+
+  it("should request with cache", (done) => {
+    app.get('/', function (req, res) {
+      res.send('ok');
+    });
+    request(app).get('/').end(function (err, res) {
+      assert(!err);
+      assert(res.text === 'ok');
+      done();
+    });
+  });
+  it("should request with cache", (done) => {
+    cache._clear('/');
+    request(app).get('/').end(function (err, res) {
+      assert(!err);
+      assert(res.text === 'ok');
+      done();
+    });
+  });
+
+  it("should request with cache", (done) => {
+    app.get('/cache', function (req, res) {
+      if (req.cache.data) {
+        return res.send(req.cache.data);
+      }
+      const user = req.session ? req.session.user : undefined;
+      req.cache.cache.set(req, user, 'hello').then(async function () {
+        let data = await req.cache.cache.get(req, user);
+        res.send('cache');
+      });
+    });
+    request(app).get('/cache').end(function (err, res) {
+      console.log(err, res.text);
+      assert(!err);
+      assert(res.text === 'cache');
+      done();
+    });
+  });
+
+  it("should get cache", (done) => {
+    request(app).get('/cache').end(function (err, res) {
+      assert(!err);
+      assert(res.text === 'hello');
+      done();
+    });
+  });
+});
